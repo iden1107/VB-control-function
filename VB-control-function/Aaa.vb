b5b2b8abe082d9dcdde89e3c8e560bae16f444ff
Kamino-Hajime <kamion.hajime@hmw.gr.jp>
2022-12-04 12:59:39 +0900

    [modify]

--- VB-control-function/Aaa.vb
+++ VB-control-function/Aaa.vb
@@ -2,8 +2,9 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'private
         'private
-        'private
-        'private
 
+
+        'company
+        'company
     End Sub
 End Class
