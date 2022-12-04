93b336ce4801f03068f39541073dfbb804802d53
Kamino-Hajime <kamion.hajime@hmw.gr.jp>
2022-12-04 14:07:15 +0900

    [codify]

--- VB-control-function/Aaa.vb
+++ VB-control-function/Aaa.vb
@@ -1,15 +1,8 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'coop
+
     End Sub
 
 
 End Class
 
-Public Class Super
-    Private name As String
-
-    Public Sub Fname()
-        Debug.WriteLine("hoge")
-    End Sub
-End Class
