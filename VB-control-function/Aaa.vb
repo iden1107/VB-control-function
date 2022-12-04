0153c007735e9d56fa129d2161de3a5825b1f18e
神農 甫 <iden1107@icloud.com>
2022-12-04 14:03:17 +0900

    [add]

--- VB-control-function/Aaa.vb
+++ VB-control-function/Aaa.vb
@@ -2,4 +2,10 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         'coop
     End Sub
+
+
+End Class
+
+Public Class Super
+    Private name As String
 End Class
