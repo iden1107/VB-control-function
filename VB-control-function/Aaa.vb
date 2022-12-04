122a2e49e4457d5c7a01360e43fed13831c52c2f
神農 甫 <iden1107@icloud.com>
2022-12-04 12:13:04 +0900

    [modify]

--- VB-control-function/Aaa.vb
+++ VB-control-function/Aaa.vb
@@ -1,7 +1,5 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        'company
-        'private
-        'private
+        '12345
     End Sub
 End Class
